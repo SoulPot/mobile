b43bf33c00c40fd96496d2afc50449be44fbac54
SandrinePatin <sandrine.patin@free.fr>
2022-05-06 19:31:58 +0200

    MyPlant_Page

--- lib/views/MyPlant_page.dart
+++ lib/views/MyPlant_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:soulpot/widgets/CardInfoPlant.dart';
 
 class MyPlantPage extends StatefulWidget {
   const MyPlantPage({Key? key}) : super(key: key);
@@ -12,15 +13,34 @@ class _MyPlantPageState extends State<MyPlantPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset("assets/images/plant1.gif",
-          height: 500.0,
-          width: 500.0,
-        )
-      )
-
+    return SafeArea(
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+                margin: EdgeInsets.only(top:10.0),
+                child: Text("Nom de la plante", style: TextStyle(fontSize: 30))
+            ),
+            Center(
+              child: Image.asset("assets/images/plant1.gif",
+              height: MediaQuery.of(context).size.height / 2,
+              width: MediaQuery.of(context).size.width,
+            )),
+            Row(
+              children: [
+                CardInfoPlant(cardType: "luminosity"),
+                CardInfoPlant(cardType: "temperature"),
+              ],
+            ),
+            Row(
+              children: [
+                CardInfoPlant(cardType: "humidity",),
+                CardInfoPlant(cardType: "analyzer",),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
-
 }
